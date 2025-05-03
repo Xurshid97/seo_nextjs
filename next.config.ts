3be096ce4a7adb4ad14e5aaa0c1cc21e48bb58c7
Xurshid97 <xurshedrabo@gmail.com>
2025-05-03 12:58:18 +0200

    image being rendered

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fakestoreapi.com",
+        pathname: "/img/**", // Allow all images under /img/ path
+      },
+    ],
+  },
 };
 
 export default nextConfig;
