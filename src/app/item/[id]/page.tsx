3926969f678285e5970b32e7c904b83be90e1529
Xurshid97 <xurshedrabo@gmail.com>
2025-05-03 12:50:08 +0200

    deployment errors

--- src/app/item/[id]/page.tsx
+++ src/app/item/[id]/page.tsx
@@ -1,36 +1,34 @@
 import Image from "next/image";
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
-interface Params {
-  params: { id: string };
-}
-
-export default async function ItemDetail({ params }: Params) {
-  const res = await fetch(`https://fakestoreapi.com/products/${params.id}`, { cache: 'no-store' });
+export default async function ItemDetail({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params; // Await the params Promise to get the id
+  const res = await fetch(`https://fakestoreapi.com/products/${id}`, { cache: "no-store" });
   const item = await res.json();
 
   return (
     <main>
-      <div style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        padding: '20px',
-        border: '1px solid #ccc',
-        borderRadius: '5px',
-        maxWidth: '400px',
-        margin: 'auto',
-      }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          padding: "20px",
+          border: "1px solid #ccc",
+          borderRadius: "5px",
+          maxWidth: "400px",
+          margin: "auto",
+        }}
+      >
         <h1 className="text-xl font-bold">{item.title}</h1>
-        <div>
-          <Image src={item.image} alt={item.title} style={{
-            width: '100%',
-            height: 'auto',
-            borderRadius: '5px',
-          }} />
-        </div>
-        
+        <Image
+          src={item.image}
+          alt={item.title}
+          width={300}
+          height={300}
+          style={{ borderRadius: "5px" }}
+        />
         <p>{item.description}</p>
         <p className="font-bold">${item.price}</p>
       </div>
